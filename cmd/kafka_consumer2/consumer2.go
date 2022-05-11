856a1ff930fbf56008d270cedb3b8a4feb6e3edc
akashdas-0111 <61954560+akashdas-0111@users.noreply.github.com>
2022-05-11 14:46:27 +0530

    updated code

--- cmd/kafka_consumer2/consumer2.go
+++ cmd/kafka_consumer2/consumer2.go
@@ -3,25 +3,26 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/segmentio/kafka-go"
 )
 
-func main(){
-	conf:=kafka.ReaderConfig{
+func main() {
+	reader := kafka.NewReader(kafka.ReaderConfig{
 		Brokers: []string{"localhost:9092"},
-		// GroupID:"two" ,
-		Topic: "testinggroup",
-		Partition: 0,
-		
-	}
-	reader:=kafka.NewReader(conf)
-	for{
-		m,err:=reader.ReadMessage(context.Background())
-		if err!=nil{
-			fmt.Println("Error",err)
+		GroupID: "two",
+		Topic:   "testinggroup",
+		// Partition: 1,
+
+	})
+
+	for {
+		m, err := reader.ReadMessage(context.Background())
+		if err != nil {
+			fmt.Println("Error", err)
 		}
 		fmt.Println(string(m.Value))
-		
+		time.Sleep(5*time.Second)
 	}
 }
--- cmd/producer22/producer2.go
+++ cmd/producer22/producer2.go
@@ -11,10 +11,13 @@ import (
 )
 
 func main() {
-	var part int
-	fmt.Println("Enter partition number")
-	fmt.Scanf("%d",part)
-	connec, _ := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", "testinggroup",part)
+
+	connec, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", "testinggroup",1)
+	if(err!=nil){
+		fmt.Println(err)
+	}else{
+		fmt.Println("Connected")
+	}
 	connec.SetDeadline(time.Now().Add(time.Second * 10))
 	fmt.Println("Enter the message")
 	scanner := bufio.NewScanner(os.Stdin)
